119217a23d8baeb28662a937d4dd2ee2fb15016c
hsiangyichen <hsiangyi1025@gmail.com>
2025-02-11 12:00:57 -0800

    feat: Set up project

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+const port = 8080;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the server!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
