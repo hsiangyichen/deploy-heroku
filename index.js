0928b62f8cd19b39987eb9e7cf851d8b5663dafe
hsiangyichen <hsiangyi1025@gmail.com>
2025-02-11 12:44:17 -0800

    feat: Set up project

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 5050;
 
 app.get("/", (req, res) => {
   res.send(`<h1>Welcome to my Express App</h1>`);
